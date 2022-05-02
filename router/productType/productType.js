4a8489f6807f60f7f2262cf9646acf0bf554d776
unknown <jaideepgrover147@gmail.com>
2022-05-02 15:54:33 +0530

    first commit

---
+++ router/productType/productType.js
@@ -0,0 +1,30 @@
+const getDb = require('../../database/database').getDb;
+const express = require('express');
+const router = express.Router()
+
+router.use('/addProductType', (req, res, next)=>{
+    let db = getDb();
+    console.log("product type")
+    const {name} = req.headers;
+    db.collection('productType').find({name: name}).toArray().then((response)=>{
+        if(response.length > 0){
+            res.send({status: 'alreadyInserted'})
+        }else{
+            db.collection('productType').insertOne({name: name}).then((response)=>{
+                let obj = {name: name}
+                res.send(obj)
+            })
+        }
+    })
+    
+})
+
+router.use('/displayProductType', (req,res, next)=>{
+    let db = getDb();
+    db.collection('productType').find({}).toArray().then((response)=>{
+        res.send(response)
+    })  
+})
+
+
+exports.productType = router
