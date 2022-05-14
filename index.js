6bc38a92a20d800e19d0f4cc97cc354b0716424e
unknown <jaideepgrover147@gmail.com>
2022-05-14 20:37:40 +0530

    first msg

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.use(remove);
 app.get('/', (req, res, next)=>{
     console.log('here we are');
     console.log(req.cookies)
-    res.send({status: 'helloLatest'})
+    res.send({status: 'helloLatest4'})
 })
 mongoConnect(() => {
     app.listen(port, ()=>{
